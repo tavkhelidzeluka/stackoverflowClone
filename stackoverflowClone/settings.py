9836e6153e6a8ab01155199fd1d88a1396bc954a
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-01-23 20:22:24 +0400

    Add profile to users

--- stackoverflowClone/settings.py
+++ stackoverflowClone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party
+    'crispy_forms',
 
     # Custom
     'forum',
@@ -123,11 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-
 class User(AbstractUser):
-    pass
+    birth_date = models.DateField(blank=True, null=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    icon = models.ImageField(upload_to='user_profile_pics/')
+
