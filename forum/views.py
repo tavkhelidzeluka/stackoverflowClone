09e63c436ff22bfca1665908a33627b67a42f673
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2023-01-23 21:31:42 +0400

    Add view update on questions

--- forum/views.py
+++ forum/views.py
@@ -24,6 +24,13 @@ class HomeView(ListView):
 
 class QuestionDetailView(DetailView):
     model = Question
+    
+    def get(self, request, *args, **kwargs):
+        self.object: Question = self.get_object()
+        self.object.views += 1
+        self.object.save()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 # def create_question_view(request):
